79eb4e81e512bc862576605747f852577689d145
DevMai90 <dmai90@yahoo.com>
2019-02-07 23:12:56 -0800

    Added controlled components. Fixed bug.

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -7,6 +7,13 @@ class AddContact extends Component {
     phone: ''
   };
 
+  onChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  onSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+
   render() {
     const { name, email, phone } = this.state;
 
@@ -14,56 +21,61 @@ class AddContact extends Component {
       <div className="card mb-3">
         <div className="card-header">Add Contact</div>
         <div className="card-body">
-          <div className="form-group">
-            <div className="input-group">
-              <div className="input-group-prepend">
-                <span className="input-group-text">Name</span>
+          <form onSubmit={this.onSubmit}>
+            <div className="form-group">
+              <div className="input-group">
+                <div className="input-group-prepend">
+                  <span className="input-group-text">Name</span>
+                </div>
+                <input
+                  type="text"
+                  className="form-control form-control-lg"
+                  name="name"
+                  placeholder="Enter Name..."
+                  value={name}
+                  onChange={this.onChange}
+                />
               </div>
-              <input
-                type="text"
-                className="form-control form-control-lg"
-                name="name"
-                placeholder="Enter Name..."
-                value={name}
-              />
             </div>
-          </div>
 
-          <div className="form-group">
-            <div className="input-group">
-              <div className="input-group-prepend">
-                <span className="input-group-text">Email</span>
+            <div className="form-group">
+              <div className="input-group">
+                <div className="input-group-prepend">
+                  <span className="input-group-text">Email</span>
+                </div>
+                <input
+                  type="email"
+                  className="form-control form-control-lg"
+                  name="email"
+                  placeholder="Enter Email..."
+                  value={email}
+                  onChange={this.onChange}
+                />
               </div>
-              <input
-                type="email"
-                className="form-control form-control-lg"
-                name="email"
-                placeholder="Enter Email..."
-                value={email}
-              />
             </div>
-          </div>
 
-          <div className="form-group">
-            <div className="input-group">
-              <div className="input-group-prepend">
-                <span className="input-group-text">Phone</span>
+            <div className="form-group">
+              <div className="input-group">
+                <div className="input-group-prepend">
+                  <span className="input-group-text">Phone</span>
+                </div>
+                <input
+                  type="text"
+                  className="form-control form-control-lg"
+                  name="phone"
+                  placeholder="Enter Phone..."
+                  value={phone}
+                  onChange={this.onChange}
+                />
               </div>
-              <input
-                type="text"
-                className="form-control form-control-lg"
-                name="phone"
-                placeholder="Enter Phone..."
-                value={phone}
-              />
             </div>
-          </div>
 
-          <input
-            type="submit"
-            className="btn btn-block btn-light"
-            value="Submit"
-          />
+            <input
+              type="submit"
+              className="btn btn-block btn-light"
+              value="Submit"
+            />
+          </form>
         </div>
       </div>
     );
