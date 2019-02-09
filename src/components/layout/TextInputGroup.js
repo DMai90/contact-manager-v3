529d11af5634a2eab7b48232f2cd7d40197b32be
DevMai90 <dmai90@yahoo.com>
2019-02-08 23:37:06 -0800

    Added error handling. Imported classnames dependency

--- src/components/layout/TextInputGroup.js
+++ src/components/layout/TextInputGroup.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
 const TextInputGroup = ({
   prepend,
@@ -7,7 +8,8 @@ const TextInputGroup = ({
   value,
   placeholder,
   type,
-  onChange
+  onChange,
+  error
 }) => {
   return (
     <div className="form-group">
@@ -17,12 +19,15 @@ const TextInputGroup = ({
         </div>
         <input
           type={type}
-          className="form-control form-control-lg"
+          className={classnames('form-control form-control-lg', {
+            'is-invalid': error
+          })}
           name={name}
           placeholder={placeholder}
           value={value}
           onChange={onChange}
         />
+        {error && <div className="invalid-feedback">{error}</div>}
       </div>
     </div>
   );
@@ -34,7 +39,8 @@ TextInputGroup.propTypers = {
   value: PropTypes.string.isRequired,
   placeholder: PropTypes.string.isRequired,
   type: PropTypes.string.isRequired,
-  onChange: PropTypes.func.isRequired
+  onChange: PropTypes.func.isRequired,
+  error: PropTypes.string.isRequired
 };
 
 TextInputGroup.defaultProps = {
