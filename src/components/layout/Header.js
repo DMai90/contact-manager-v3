ccb41e72a468b44ab1bfe9ef75a806f880064184
DevMai90 <dmai90@yahoo.com>
2019-02-11 22:20:15 -0800

    Refactored code to use async/await

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -7,9 +7,9 @@ const Header = props => {
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0">
       <div className="container">
-        <a href="/" className="navbar-brand">
+        <Link to="/" className="navbar-brand">
           {branding}
-        </a>
+        </Link>
         <button
           className="navbar-toggler"
           data-toggle="collapse"
