0293f66abc5f3d0a8b6aa8921c483f0b2c252fdd
DevMai90 <dmai90@yahoo.com>
2019-02-12 23:46:49 -0800

    Added EditContact component

---
+++ src/components/contacts/EditContact.js
@@ -0,0 +1,123 @@
+import React, { Component } from 'react';
+import { Consumer } from '../../context';
+import TextInputGroup from '../layout/TextInputGroup';
+import axios from 'axios';
+
+class EditContact extends Component {
+  state = {
+    name: '',
+    email: '',
+    phone: '',
+    errors: {}
+  };
+
+  async componentDidMount() {
+    const { id } = this.props.match.params;
+    const res = await axios.get(
+      `https://jsonplaceholder.typicode.com/users/${id}`
+    );
+
+    // const contact = res.data;
+    const { name, email, phone } = res.data;
+
+    this.setState({
+      name,
+      email,
+      phone
+    });
+  }
+
+  onChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  onSubmit = async (dispatch, e) => {
+    e.preventDefault();
+
+    const { name, email, phone } = this.state;
+
+    // Check Input Fields
+    if (name === '') {
+      this.setState({ errors: { name: 'Name is required' } });
+      return;
+    }
+    if (email === '') {
+      this.setState({ errors: { email: 'Email is required' } });
+      return;
+    }
+    if (phone === '') {
+      this.setState({ errors: { phone: 'Phone is required' } });
+      return;
+    }
+
+    // Clear State
+    this.setState({
+      name: '',
+      email: '',
+      phone: '',
+      errors: {}
+    });
+
+    // Redirect
+    this.props.history.push('/');
+  };
+
+  render() {
+    const { name, email, phone, errors } = this.state;
+
+    return (
+      <Consumer>
+        {value => {
+          const { dispatch } = value;
+          return (
+            <React.Fragment>
+              <h1 className="display-4 mb-2">
+                <span className="text-danger">Edit </span>Contact
+              </h1>
+              <div className="card mb-3">
+                <h4 className="card-header">Contact Information</h4>
+                <div className="card-body">
+                  <form onSubmit={this.onSubmit.bind(this, dispatch)}>
+                    <TextInputGroup
+                      prepend="Name"
+                      name="name"
+                      placeholder="Enter Name..."
+                      value={name}
+                      onChange={this.onChange}
+                      error={errors.name}
+                    />
+
+                    <TextInputGroup
+                      prepend="Email"
+                      name="email"
+                      type="email"
+                      placeholder="Enter Email..."
+                      value={email}
+                      onChange={this.onChange}
+                      error={errors.email}
+                    />
+
+                    <TextInputGroup
+                      prepend="Phone"
+                      name="phone"
+                      placeholder="Enter Phone..."
+                      value={phone}
+                      onChange={this.onChange}
+                      error={errors.phone}
+                    />
+
+                    <input
+                      type="submit"
+                      className="btn btn-block btn-light"
+                      value="Update Contact"
+                    />
+                  </form>
+                </div>
+              </div>
+            </React.Fragment>
+          );
+        }}
+      </Consumer>
+    );
+  }
+}
+
+export default EditContact;
