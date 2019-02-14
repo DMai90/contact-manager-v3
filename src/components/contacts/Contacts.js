14d8792ca93db38bc5e6fbb527a88280b7e93242
DevMai90 <dmai90@yahoo.com>
2019-02-13 23:56:39 -0800

    Add Contact Button

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Contact from './Contact';
 import { Consumer } from '../../context';
+import { Link } from 'react-router-dom';
 
 class Contacts extends Component {
   render() {
@@ -10,9 +11,21 @@ class Contacts extends Component {
           const { contacts } = value;
           return (
             <React.Fragment>
-              <h1 className="display-4 mb-2">
-                <span className="text-danger">Contact</span> List
-              </h1>
+              <div className="d-flex row-hl">
+                <div className="item-hl">
+                  <h1 className="display-4 mb-2">
+                    <span className="text-danger">Contact</span> List
+                  </h1>
+                </div>
+                <div className="item-hl ml-auto pt-3">
+                  <Link to="/contact/add">
+                    <button className="btn btn-danger">
+                      <i className="fas fa-plus" /> Add New Contact
+                    </button>
+                  </Link>
+                </div>
+              </div>
+
               {contacts.map(contact => (
                 <Contact key={contact.id} contact={contact} />
               ))}
