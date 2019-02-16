0db59a4f1a5e84c67263780fecea533def53da8e
DevMai90 <dmai90@yahoo.com>
2019-02-15 22:50:47 -0800

    Updates

---
+++ precache-manifest.a7e59d7019c58ddadc9b2fb723268846.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42c20b20e558b9cc9061",
+    "url": "/contact-manager-v3/static/js/runtime~main.42c20b20.js"
+  },
+  {
+    "revision": "15a04d894fe5ad7f765e",
+    "url": "/contact-manager-v3/static/js/main.15a04d89.chunk.js"
+  },
+  {
+    "revision": "6aacdd67f9a1f1d617a3",
+    "url": "/contact-manager-v3/static/js/1.6aacdd67.chunk.js"
+  },
+  {
+    "revision": "15a04d894fe5ad7f765e",
+    "url": "/contact-manager-v3/static/css/main.166199d8.chunk.css"
+  },
+  {
+    "revision": "6aacdd67f9a1f1d617a3",
+    "url": "/contact-manager-v3/static/css/1.81e550b0.chunk.css"
+  },
+  {
+    "revision": "68f6483e91f9a7fe078e9c3108ae4979",
+    "url": "/contact-manager-v3/index.html"
+  }
+];
